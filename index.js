f4e40c50adcc7d3258b2943e199e96ed5ebb11d0
Merlyn <46276713+MerlynMShelley@users.noreply.github.com>
2022-10-21 14:08:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function increment() {
 }
 
 function save() {
-    let countStr = count + " - "
+    let countStr = " - " + count
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
